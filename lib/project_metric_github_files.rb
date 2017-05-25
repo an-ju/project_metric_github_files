cc89ba87dd37ea748fb027189513d2282e812cbc
An Ju <an_ju@berkeley.edu>
2017-05-25 15:01:35 -0700

    First commit.

---
+++ lib/project_metric_github_files.rb
@@ -0,0 +1,7 @@
+require "project_metric_github_files/version"
+require "octokit"
+require "json"
+
+class ProjectMetricGithubFiles
+  # Your code goes here...
+end
---
+++ lib/project_metric_github_files/version.rb
@@ -0,0 +1,3 @@
+class ProjectMetricGithubFiles
+  VERSION = "0.1.0"
+end
