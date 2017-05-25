0f8397b893d38c78e8f33b40ed5fb610f51c3545
An Ju <an_ju@berkeley.edu>
2017-05-25 16:07:26 -0700

    Version 0.1.0

--- lib/project_metric_github_files.rb
+++ lib/project_metric_github_files.rb
@@ -1,7 +1,79 @@
 require "project_metric_github_files/version"
 require "octokit"
 require "json"
+require "open-uri"
+require "date"
 
 class ProjectMetricGithubFiles
-  # Your code goes here...
+  attr_reader :raw_data
+
+  def initialize(credentials, raw_data = nil)
+    @project_url = credentials[:github_project]
+    @identifier = URI.parse(@project_url).path[1..-1]
+    @client = Octokit::Client.new access_token: credentials[:github_access_token]
+    @client.auto_paginate = true
+
+    @raw_data = raw_data
+  end
+
+  def refresh
+    @raw_data = commits.map(&:to_h)
+    @score = @image = nil
+  end
+
+  def raw_data=(new)
+    @raw_data = new
+    @score = @image = nil
+  end
+
+  def score
+    @raw_data ||= commits
+    synthesize
+    @score = @type_lines.each_value.reduce(&:+)
+  end
+
+  def image
+    @raw_data ||= commits
+    synthesize
+    @image ||= { chartType: 'commit_flow',
+                 titleText: 'Commit Flow',
+                 data: @type_lines }.to_json
+  end
+
+  def self.credentials
+    %I[github_project github_access_token]
+  end
+
+  private
+
+  def commits
+    @client.commits_since(@identifier, Date.today - 7).map do |cmit|
+      @client.commit(@identifier, cmit[:sha])
+    end
+  end
+
+  def synthesize
+    @raw_data ||= commits
+    @type_files = @raw_data.flat_map { |cmit| cmit[:files] }.group_by { |f| file_type f }
+    @type_lines = {}
+    @type_files.each_pair do |key, val|
+      @type_lines[key] = val.map { |cmit| cmit[:changes] }.reduce(&:+)
+    end
+  end
+
+  def file_type(f)
+    if %r{^app/model} =~ f[:filename]
+      :model
+    elsif %r{^app/controller} =~ f[:filename]
+      :controller
+    elsif (%r{^app/view} =~ f[:filename]) || (%r{^app/assets} =~ f[:filename])
+      :view
+    elsif (%r{^spec/} =~ f[:filename]) || (%r{^cucumber/} =~ f[:filename])
+      :test
+    elsif %r{^db/} =~ f[:filename]
+      :db
+    else
+      :other
+    end
+  end
 end
